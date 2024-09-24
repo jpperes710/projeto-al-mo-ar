a89d88fd06f2af3b66f7cb389016d14a138a7a01
Joao pedro Peres soares <jp.galomgperes@gmail.com>
2024-09-24 10:27:13 -0300

    projeto

---
+++ HTML/script.js
@@ -0,0 +1,24 @@
+import { times } from "./data.js";
+
+const tbodyJogadores = document.getElementById('tbody-jogadores');
+const inputPesquisar = document.getElementById('input-pesquisar');
+const btnPesquisar = document.getElementById('btn-pesquisar');
+
+const alMocar = times[0];
+
+for (const jogador of alMocar.jogadores) {
+    tbodyJogadores.innerHTML += `
+        <tr>
+            <td>${jogador.posicao}</td>
+            <td>${jogador.nome}</td>
+            <td>${jogador.idade}</td>
+            <td>${jogador.nivel}</td>
+            <td>${jogador.preco}</td>
+            
+        </tr> `
+};
+
+
+btnPesquisar.addEventListener('click', () => {
+    alert(inputPesquisar.value);
+})
